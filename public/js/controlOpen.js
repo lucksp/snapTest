ef9baec9518274a170eee2b2a6e8a8c3ac6906b7
Phil Lucks <lucksp@gmail.com>
2016-03-22 00:50:55 -0600

    working on chat modals

--- public/js/controlOpen.js
+++ public/js/controlOpen.js
@@ -4,17 +4,39 @@ angular.module('snapTest')
     	factorySnap.get().then(function(responseData){
     		$scope.snapData = responseData.data
 
+    		$scope.chatData = []
+    		for(var i = 0; i < $scope.snapData.length; i++){
+    			if($scope.snapData[i].type === "chat"){
+    				$scope.chatData.push($scope.snapData[i])
+    			}
+    		}
     	})
 
-		$scope.currentPage = 1;
-		$scope.pageSize = 25;
+		$scope.currentPage 	= 1;
+		$scope.pageSize 	= 25;
 
 		$scope.pageChangeHandler = function(num) {
 		      // console.log('snapData per page changed to: ' + num);
 		  }
 
 		$scope.pageChangeHandler = function(num) {
-		    console.log('going to page ' + num);
+		    // console.log('going to page ' + num);
+		}
+
+		$scope.sortType     = 'created'
+		$scope.sortReverse  = false
+
+
+		var markers = [];
+		$scope.modalDetails = function(snap) {
+			$scope.selectedUser = snap	
+		}
+		$scope.runModalMap = function(snap) {
+			markers.push({
+					focus: true,
+	                lat: $scope.selectedUser.latitude,
+	                lng: $scope.selectedUser.longitude
+	            });
 		}
 
 }]);
