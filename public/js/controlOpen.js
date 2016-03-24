77cf4692c91e45588b20fc8913bac10095ce1305
Phil Lucks <lucksp@gmail.com>
2016-03-24 11:42:26 -0600

    completed geo

--- public/js/controlOpen.js
+++ public/js/controlOpen.js
@@ -1,10 +1,5 @@
 angular.module('snapTest')
-    .controller('controlOpen',['$scope', '$http', 'factorySnap', function($scope, $http, factorySnap){
-
-
-	// angular.extend($scope, {
-		
- //            })
+    .controller('controlOpen',['$scope', '$http', '$timeout', 'leafletData', 'factorySnap', function($scope, $http, $timeout, leafletData, factorySnap){
 
     	factorySnap.get().then(function(responseData){
     		$scope.snapData = responseData.data
@@ -37,17 +32,42 @@ angular.module('snapTest')
 		var lat			= 0
 		var lng 		= 0
 
-		// var markers = [];
+		$scope.markers = [];
+		$scope.showMap = false;
 		$scope.modalDetails = function(snap) {
+			$scope.showMap = !$scope.showMap
 			$scope.selectedUser = snap
-
 			waitTime += $scope.selectedUser.chat_waittime
 			duration += $scope.selectedUser.chat_duration
 
+			angular.extend($scope, {
+					center: {
+						lat: $scope.selectedUser.latitude,
+						lng: $scope.selectedUser.longitude,
+						zoom: 8
+					}
+			})
+            $scope.$watch("showMap", function(value) {
+                if (value === true) {
+                    leafletData.getMap().then(function(map) {
+                      $timeout(function() {
+                        map.invalidateSize();
+                      }, 300);
+                    });
+                }
+            });
+			
+			$scope.markers.push({
+								lat: $scope.selectedUser.latitude,
+								lng: $scope.selectedUser.longitude,
+								focus: true
+								})
+
+			console.log('selectedUser: ', $scope.selectedUser.latitude, $scope.selectedUser.longitude)
+
 			$scope.userWaitTime = Math.floor(waitTime/60) + ':' + waitTime%60
 			$scope.userDuration = Math.floor(duration/60) + ':' + duration%60
-		}		
-
+		}
 }]);
 
 angular.module('snapTest')
